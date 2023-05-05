198080eb82d909a5fc05f8131b5bcd741b38f865
Fabiann08 <Fabianjose64@gmail.com>
2023-05-04 23:30:21 -0400

    Completed code... template classes give linker errors when in implementation files

--- DS_8.1_ListQueue/ListQueue.h
+++ DS_8.1_ListQueue/ListQueue.h
@@ -28,10 +28,54 @@ public:
 
 	/** @throw  PrecondViolatedExcep if queue is empty. */
 	ItemType peekFront() const throw(PrecondViolatedExcep);
-
-	//bool isPalindrome(string someString);
-
 }; // end ListQueue
 
-//#include "ListQueue.cpp"
+
+template<class ItemType>
+ListQueue<ItemType>::ListQueue()
+{
+	listPtr = new LinkedList<ItemType>();
+}  // end default constructor
+
+template<class ItemType>
+ListQueue<ItemType>::ListQueue(const ListQueue& aQueue) : listPtr(aQueue.listPtr)
+{
+}  // end copy constructor
+
+template<class ItemType>
+ListQueue<ItemType>::~ListQueue()
+{
+}  // end destructor
+
+template<class ItemType>
+bool ListQueue<ItemType>::isEmpty() const
+{
+	return listPtr->isEmpty();
+}  // end isEmpty
+
+template<class ItemType>
+bool ListQueue<ItemType>::enqueue(const ItemType& newEntry)
+{
+	return listPtr->insert(listPtr->getLength() + 1, newEntry);
+}  // end enqueue
+
+template<class ItemType>
+bool ListQueue<ItemType>::dequeue()
+{
+	return listPtr->remove(1);
+}  // end dequeue
+
+template<class ItemType>
+ItemType ListQueue<ItemType>::peekFront() const throw(PrecondViolatedExcep)
+{
+	if (isEmpty())
+		throw PrecondViolatedExcep("peekFront() called with empty queue.");
+
+	// Queue is not empty; return front
+	return listPtr->getEntry(1);
+}  // end peekFront
+
+// End of implementation file.
+
+
 #endif
--- DS_8.1_ListQueue/Main.cpp
+++ DS_8.1_ListQueue/Main.cpp
@@ -1,67 +1,73 @@
 #include <iostream>
-#include <string>
-//#include "PrecondViolatedExcep.h"
-#include "PalindromeChecker.h"
-//#include "ListQueue.h"
+#include "ListQueue.h"
+#include "LinkedStack.h"
+
 using namespace std;
+
+bool isPalindrome(string someString);
 
 int main()
 {
-    string str1 = "abcda";
-    string str2 = "radar";
-
-    if (PalindromeChecker::isPalindrome(str1)) {
-        cout << str1 << " is a palindrome." << endl;
+    // Test the isPalindrome function
+    string testString = "abcda";
+    if (isPalindrome(testString))
+    {
+        cout << testString << " is a palindrome" << endl;
+    }
+    else
+    {
+        cout << testString << " is not a palindrome" << endl;
     }
 
-    else {
-        cout << str1 << " is not a palindrome." << endl;
+    string testString2 = "radar";
+    if (isPalindrome(testString2))
+    {
+        cout << testString2 << " is a palindrome" << endl;
     }
-
-    if (PalindromeChecker::isPalindrome(str2)) {
-        cout << str2 << " is a palindrome." << endl;
+    else
+    {
+        cout << testString2 << " is not a palindrome" << endl;
     }
-    else {
-        cout << str2 << " is not a palindrome." << endl;
-    }
-    /*
-    string str1 = "abcda";
-    string str2 = "radar";
-
-    if (isPalindrome(str1)) {
-        cout << str1 << " is a palindrome." << endl;
-    }
-    else {
-        cout << str1 << " is not a palindrome." << endl;
-    }
-
-    if (isPalindrome(str2)) {
-        cout << str2 << " is a palindrome." << endl;
-    }
-    else {
-        cout << str2 << " is not a palindrome." << endl;
-    }
-    
-    
-    
-    ListQueue<char> queue;
-
-    string str1 = "abcda";
-    string str2 = "radar";
-
-    if (queue.isPalindrome(str1)) {
-        cout << "1 is a palindrome." << endl;
-    }
-    else {
-        cout << "1 is not a palindrome." << endl;
-    }
-
-    if (queue.isPalindrome(str2)) {
-        cout << "2 is a palindrome." << endl;
-    }
-    else {
-        cout << "2 is not a palindrome." << endl;
-    }*/
-    
 	return 0;
+}
+
+
+bool isPalindrome(string someString)
+{
+    ListQueue<char> aQueue;
+    LinkedStack<char> aStack;
+
+    // Add first length/2 characters to queue
+    int length = someString.length();
+    for (int i = 0; i < length / 2; i++)
+    {
+        char nextChar = someString[i];
+        aQueue.enqueue(nextChar);
+    }
+
+    // Push remaining characters onto stack
+    for (int i = length / 2; i < length; i++)
+    {
+        char nextChar = someString[i];
+        aStack.push(nextChar);
+    }
+
+    // Compare the queue characters with the stack characters
+    bool charactersAreEqual = true;
+    while (!aQueue.isEmpty() && charactersAreEqual)
+    {
+        char queueFront = aQueue.peekFront();
+        char stackTop = aStack.peek();
+        if (queueFront == stackTop)
+        {
+            aQueue.dequeue();
+            aStack.pop();
+        }
+        else
+        {
+            charactersAreEqual = false;
+        }
+    }
+
+    return charactersAreEqual;
 }
